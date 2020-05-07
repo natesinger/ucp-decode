c4c5967fe065bc36b764de464896c87c64c3162e
Nate Singer <nathaniel@singer.cloud>
2020-05-07 05:00:53 -0500

    pip information array update

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup
 setup(
     name = 'ucp-decode',
     version = '1.0.0',
+    description = 'Python parser for interpreting keyboard captures, under USB device class definition HID 1.11.',
+    url = 'https://github.com/nmsinger/ucp-decode.git',
+    author = 'Nate Singer',
+    author_email = 'nathaniel@singer.cloud',
+    license = 'Apache 2.0',
     packages = ['ucp-decode'],
     python_requires='>=3.5',
     entry_points = {
